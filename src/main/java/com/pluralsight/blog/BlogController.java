3f20b37135eca81644a4eeb8ab905bc0b886f9c1
LAS1K <lodkarad@gmail.com>
2022-11-20 13:52:58 +0100

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -2,12 +2,12 @@ package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
         modelMap.put("title", "Blog Post 1");
         return "home";
